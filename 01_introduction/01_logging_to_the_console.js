1e8d07a9d8dbb736648f57f62de14de9c4018add
Craig Eaton <craigea92@gmail.com>
2024-03-06 16:51:33 +0000

    js 01 complete

---
+++ 01_introduction/01_logging_to_the_console.js
@@ -0,0 +1,25 @@
+/*
+LOGGING TO THE CONSOLE IN JS
+
+LET'S BUILD THE BACK-END OF A MOVIE REVIEW APP
+For the next few chapters we'll create the building blocks of a back-end web server. 
+The purpose of the server will be to supply up-to date information about the state of our Movie review mobile app: "MovieStarz". 
+For example, the server will:
+
+Store user information (emails, names, passwords)
+Provide authentication mechanisms
+Store movie reviews
+Recommend movies based on choices
+Etc.
+
+PRINTING TO THE CONSOLE IN JAVASCRIPT
+In Python, we used print() to print text. In JavaScript, we use console.log(), and we typically denote strings using single quotes:
+
+console.log('this text will be printed')
+*/
+
+// Assignment
+// Print Starting MovieStarz server to the console.
+
+// Solution
+console.log("Starting MovieStarz server");
