67d3f205d2d309b73397e802f7902ab7bb99e88d
Craig Eaton <craigea92@gmail.com>
2024-03-07 09:17:16 +0000

    js 05 complete

---
+++ 01_introduction/05_multiline_comments.js
@@ -0,0 +1,20 @@
+/*
+JAVASCRIPT MULTI-LINE COMMENTS
+
+JavaScript has a separate syntax for comments that span multiple lines. 
+It's quite convenient to not have to type // at the start of every line when you want a 3-paragraph comment!
+
+Multiline comments start with /* and are terminated with a */
+
+// Assignment
+// Comment out the block of explanation text.
+
+// Solution
+/*
+below we print the name of 
+the first active user of the
+MovieStarz application
+*/
+
+console.log('John Doe')
+
