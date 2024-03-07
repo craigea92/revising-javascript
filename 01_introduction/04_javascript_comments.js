7ca2974c8aa4447bb1f044521364e3fe0cda6698
Craig Eaton <craigea92@gmail.com>
2024-03-07 09:11:34 +0000

    js 04 complete

---
+++ 01_introduction/04_javascript_comments.js
@@ -0,0 +1,18 @@
+/*
+JAVASCRIPT COMMENTS
+
+Comments are non-executable text in your code, as a note or reminder to yourself and other developers. 
+Comments are simply a form of documentation.
+
+Comments are ignored by the JavaScript interpreter when your code runs.
+
+// this code prints a greeting
+console.log('hello world') // you can also use comments inline
+*/
+
+// Assignment
+// Comment out the first line of code
+
+// Solution
+// this prints the name of the app
+console.log('Starting MovieStarz server')
