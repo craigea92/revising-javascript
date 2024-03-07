fe7d663d070dbc512ac7a08269e059b0bf1cf2d8
Craig Eaton <craigea92@gmail.com>
2024-03-07 09:08:38 +0000

    js 02 and 03 complete

---
+++ 01_introduction/02_03_what_is_javascript.js
@@ -0,0 +1,31 @@
+/*
+
+WHAT IS JAVASCRIPT?
+
+JavaScript is famously known as the language of web browsers. JavaScript powers the dynamic behavior on nearly all websites. 
+While it originally was only used for website development, the language has continued to evolve and grow over time.
+
+Today, JavaScript is a powerful and fast programming language used by professionals for almost any kind of programming you can imagine. 
+ */
+
+// Question 01
+
+// JavaScript was originally used only in _ ?
+
+// Websites
+// School
+// Mathematics
+// Bitcoin
+
+// Answer = Websites
+
+// Question 02
+
+// Today, JavaScript is used for _ ?
+
+// Websites
+// Nothing
+// Education only
+// Almost any kind of programming
+
+// Answer = Almost any kind of programming
