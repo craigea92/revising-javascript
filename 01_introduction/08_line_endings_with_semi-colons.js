5328de8ce63bb85316414315ae0a7bfcc13d487b
Craig Eaton <craigea92@gmail.com>
2024-03-07 09:45:32 +0000

    js 08 complete

---
+++ 01_introduction/08_line_endings_with_semi-colons.js
@@ -0,0 +1,17 @@
+/**
+LINE ENDINGS WITH SEMI-COLONS
+
+In languages like C, C++, and Java, a semicolon is used as a terminator in various statements. 
+
+For example, in JavaScript:
+console.log('hello world');
+
+Notice the ; at the end of the line. In JavaScript, semicolons are optional as a terminating character. 
+Deciding whether or not to use them is up to you or the team you're working on.
+ */
+
+// Assignment
+// Fix the error on a single line by adding a semicolon as a separating character.
+
+// Solution
+console.log("write"); console.log("reviews");
