3a406fb2fb655e3a83a10480b70575d9f7d5e53a
Craig Eaton <craigea92@gmail.com>
2024-03-07 11:31:05 +0000

    js 09 complete

---
+++ 02_variables/01_declaring_variables_with_let.js
@@ -0,0 +1,17 @@
+/*
+DECLARING VARIABLES WITH "LET"
+
+In JavaScript the let keyword is used to create new variables. 
+We've already written some strings directly in console.log statements, so now lets practice using them as variables.
+
+let name = 'John'
+console.log(name)
+// prints: John
+*/
+
+// Assignment
+// Declare a new variable called email and set it to the string johndoe@gmail.com.
+
+// Solution
+let email = 'johndoe@gmail.com'
+console.log(email)
