dcaab0f362eac11c97e7c1624669f980142f01c1
Craig Eaton <craigea92@gmail.com>
2024-03-07 09:42:21 +0000

    js 06 and 07 complete

---
+++ 01_introduction/06_07_java_vs_javascript.js
@@ -0,0 +1,31 @@
+/*
+JAVA VS JAVASCRIPT
+It's a very common misconception that Java and JavaScript are the same, or even that they are similar languages. 
+The famous example that is usually given to demonstrate how untrue that is goes like this:
+
+Java is to JavaScript as car is to carpet
+
+Java and JavaScript are not the same, and they aren't really even used in the same ways.
+
+JavaScript and Java are not the same
+
+Java is older, and JavaScript was named after Java in an attempt to ride Java's wave of popularity
+ */
+
+// Question 01
+
+// JavaScript is just another name for Java
+
+// True
+// False
+
+// Answer = False
+
+// Question 02
+
+// JavaScript came ____ Java
+
+// Before
+// After
+
+// Answer = After
