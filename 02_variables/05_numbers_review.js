8f194951f58d6e8bb71e4c868a60ba3db04dd6d1
Craig Eaton <craigea92@gmail.com>
2024-03-08 15:03:54 +0000

    js 13 complete

---
+++ 02_variables/05_numbers_review.js
@@ -0,0 +1,33 @@
+/*
+NUMBERS REVIEW
+
+Remember, in JavaScript all numbers are just "number" types. 
+There is no distinction between "Float" types and "Integer" types.
+
+ADDITION
+2 + 1
+// 3
+
+SUBTRACTION
+2 - 1
+// 1
+
+MULTIPLICATION
+2 * 2
+// 4
+
+DIVISION
+3 / 2
+// 1.5 (still just a "number")
+*/
+
+// Question 01
+
+// In JavaScript 5 is a ____ while 5.5 is a ____
+
+// Number, Number
+// Integer, Number
+// Integer, Float
+// Float, Integer
+
+// Answer = Number, Number
