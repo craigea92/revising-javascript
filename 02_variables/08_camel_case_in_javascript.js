c6ee26619e5bb402cbceefb4282a7a15bc8ecb61
Craig Eaton <craigea92@gmail.com>
2024-03-08 15:46:12 +0000

    js 16 complete

---
+++ 02_variables/08_camel_case_in_javascript.js
@@ -0,0 +1,23 @@
+/*
+CAMEL CASE IN JAVASCRIPT
+
+In JavaScript, the convention we encourage is to use camelCase.
+
+Here are some casing examples:
+
+camelCase
+snake_case
+PascalCase
+SCREAMING_SNAKE_CASE
+
+We aren't choosing these casing rules arbitrarily - the JavaScript community tends to prefer camelCase.
+*/
+
+// Question 01
+
+// Camel case is the preferred convention for variable names in JavaScript
+
+// True
+// False
+
+// Answer = True
