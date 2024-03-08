c50cdc272a0a70c1df7ed203d783eca14cdb496b
Craig Eaton <craigea92@gmail.com>
2024-03-08 15:13:16 +0000

    js 14 complete

---
+++ 02_variables/06_plus_plus.js
@@ -0,0 +1,29 @@
+/*
+PLUS PLUS
+
+To increment a variable we can use the += operator. 
+JS also has a ++ operator for when you only want to increment by 1.
+
+let starRating = 4
+
+starRating += 1
+// starRating is now 5
+
+starRating++
+// starRating is now 6
+
+starRating += 4
+// starRating is now 10
+*/
+
+// Assignment
+// A user added a new movie to our movie review app! 
+// Use the ++ operator in between the comments to increment the numberOfMoviesInApp.
+
+// Solution
+let numberOfMoviesInApp = 156
+
+numberOfMoviesInApp++
+
+console.log(numberOfMoviesInApp)
+
