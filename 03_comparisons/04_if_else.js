ab2d16a2024e3637c982567c1c99c75bf714a507
Craig Eaton <craigea92@gmail.com>
2024-03-10 11:32:40 +0000

    js 26 complete

---
+++ 03_comparisons/04_if_else.js
@@ -0,0 +1,32 @@
+/*
+IF/ELSE
+
+Refer to the following examples and answer the questions.
+*/
+
+// EXAMPLE 1
+if (condition) {
+
+} else if (condition) {
+
+} else {
+
+}
+
+// EXAMPLE 2
+if (condition) {
+
+} elif (condition) {
+
+} else {
+
+}
+
+// Question 01
+
+// Which example contains valid JavaScript?
+
+// Example 1
+// Example 2
+
+// Answer = Example 1
