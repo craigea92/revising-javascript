3d035b41bc4dde5ee5d6d5d03fa51e620a71d331
Craig Eaton <craigea92@gmail.com>
2024-03-12 11:32:40 +0000

    js 28 complete

---
+++ 03_comparisons/06_when_to_use_a_ternary.js
@@ -0,0 +1,20 @@
+/*
+WHEN TO USE A TERNARY
+
+You will use if/else statements more often than ternaries, generally speaking.
+
+Ternary operations are great for small, single-line conditionals. 
+
+I've seen developers in the professional world get "clever" with ternaries, and start creating nested monstrosities like this:
+
+const vehicleName = isTruck ? 'truck' : isCar ? 'car' : isScooter ? 'scooter' : 'vehicle';
+*/
+
+// Question 01
+
+// Use ternaries when the logic is ____
+
+// Simple
+// Complex
+
+// Answer = Simple
