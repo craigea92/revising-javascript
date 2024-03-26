65d675dded48fc292f7039a63bd786980c75d383
Craig Eaton <craigea92@gmail.com>
2024-03-26 11:32:40 +0000

    js 38, 39 and 40 complete

---
+++ 04_functions/10_11_12_scope_quiz.js
@@ -0,0 +1,47 @@
+/*
+SCOPE QUIZ
+
+const truck = 'CyberTruck'
+
+function getCar() {
+  const car = 'Volvo';
+}
+
+function getScooter() {
+  const scooter = 'Razor';
+
+  function getBike() {
+    const bike = 'Huffy';
+  }
+}
+
+*/
+
+// Question 01
+
+// Is the 'bike' variable available within the entire getScooter function?
+
+// Yes
+// No
+
+// Answer = No
+
+// Question 02
+
+// Is the 'car' variable available within the global scope?
+
+// Yes
+// No
+
+// Answer = No
+
+// Question 03
+
+// Which variables are available within the entire getBike function?
+
+// truck, scooter and bike
+// scooter and bike
+// car, scooter and bike
+// bike
+
+// Answer = truck, scooter and bike
