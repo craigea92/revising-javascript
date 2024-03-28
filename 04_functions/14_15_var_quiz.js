fa84962076ba175a7639d363cc8ecfbd0d149b4a
Craig Eaton <craigea92@gmail.com>
2024-03-28 11:32:40 +0000

    js 42 and 43 complete

---
+++ 04_functions/14_15_var_quiz.js
@@ -0,0 +1,24 @@
+/*
+VAR QUIZ
+
+*/
+
+// Question 01
+
+// Is there a time to use var over let?
+
+// Yes
+// Almost certainly not
+
+// Answer = Almost certainly not
+
+// Question 02
+
+// Avoid var because ____
+
+// The way it deals with scoping is non-intuitive
+// It's slower
+// 'var' was a poor choice of keyword
+// 'let' is new, and new is good
+
+// Answer = The way it deals with scoping is non-intuitive
