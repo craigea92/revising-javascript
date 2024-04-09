a79b965bd78506b8f7fdd1c528a7771b3d89d332
Craig Eaton <craigea92@gmail.com>
2024-04-09 11:32:40 +0100

    js 50 and 51 complete

---
+++ 05_arrays/06_07_modifying_constant_arrays.js
@@ -0,0 +1,33 @@
+/*
+MODIFYING CONST ARRAYS
+
+It's important to note that const arrays can still be modified, they just can't be reassigned. 
+That means we can add and remove elements, but we can't set a new array value with the assignment operator: =.
+
+const drinks = []
+
+// this works
+drinks.push('lemonade')
+
+// this breaks
+drinks = ['root beer']
+
+*/
+
+// Question 01
+
+// Can the elements of a const array ever be changed?
+
+// Yes
+// No
+
+// Answer = Yes
+
+// Question 02
+
+// The assignment operator (=) can be used to re-set the value of a const array.
+
+// True
+// False
+
+// Answer = False 
